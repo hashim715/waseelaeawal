3b15a8920150eaa9bdd12d7e4d7e4a98a167ad78
hashim715 <mharus575@gmail.com>
2022-12-01 22:57:54 +0500

    changed

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn waseelaeawal.wsgi
--- waseelaeawal/settings.py
+++ waseelaeawal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2+y(p6-s+@ekkbwh7(27&&of0em&y(jv&x)5xltx0nlq)jxqb$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.43.41','127.0.0.1','localhost']
 
 
 # Application definition
@@ -121,6 +121,7 @@ STATIC_URL = '/static/'
 STATICFILE_DIRS=[
     os.path.join(BASE_DIR,"static")
 ]
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
